8fb86ed15b4bf0efb7fdf136bc9c7509245bae48
isabela santos <isabelatravaglia@gmail.com>
2021-09-17 16:05:18 +0000

    added chi

--- article/main.go
+++ article/main.go
@@ -3,18 +3,36 @@ package main
 import (
   "fmt"
   "net/http"
-  "github.com/gorilla/mux"
 )
 
-type handlePage struct {}
+type HandlePage struct {}
 
-func (h handlePage) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+func (h HandlePage) ServeHTTP(w http.ResponseWriter, r *http.Request) {
   w.Header().Set("Content-Type", "text/html")
   fmt.Fprint(w, "<h1>Welcome to the handle page!</h1>")
 }
 
+func HandleFuncPage(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+  fmt.Fprint(w, "<h1>Welcome to the handlefunc page!</h1>")
+}
+
+func HandleFuncConvertedPage(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+  fmt.Fprint(w, "<h1>Welcome to the handlefunc converted page!</h1>")
+}
+
+
+
 func main() {
-  r := mux.NewRouter()
-  r.Handle("/handlepage", new(handlePage))
-  http.ListenAndServe(":3000", r)
+	var checktype http.HandlerFunc
+	checktype = HandleFuncPage
+
+	NewHandlePage := new(HandlePage)
+	// HandleFuncConvPage := http.HandlerFunc(HandleFuncConvertedPage)
+	
+  http.Handle("/handlepage", NewHandlePage)
+	http.HandleFunc("/handlefuncpage", HandleFuncPage)
+	http.Handle("/handlefuncconvertedpage", checktype)
+  http.ListenAndServe(":3000", nil)
 }
--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/isabelatravaglia/go-link-parser
 go 1.16
 
 require (
-	github.com/gorilla/mux v1.8.0 // indirect
+	github.com/go-chi/chi/v5 v5.0.4 // indirect
+	github.com/gorilla/mux v1.8.0
 	golang.org/x/net v0.0.0-20210813160813-60bc85c4be6d
 )
