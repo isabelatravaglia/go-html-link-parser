b10813a225eef352e72d4009c0c4cd04ce31ea2f
isabela santos <isabelatravaglia@gmail.com>
2021-10-07 14:35:57 +0000

    added new packages. created views for home, contact and faq pages

--- go.mod
+++ go.mod
@@ -5,5 +5,8 @@ go 1.16
 require (
 	github.com/go-chi/chi/v5 v5.0.4 // indirect
 	github.com/gorilla/mux v1.8.0
+	github.com/rogpeppe/godef v1.1.2 // indirect
+	golang.org/x/crypto v0.0.0-20200622213623-75b288015ac9 // indirect
 	golang.org/x/net v0.0.0-20210813160813-60bc85c4be6d
+	golang.org/x/tools v0.1.6 // indirect
 )
--- main.go
+++ main.go
@@ -1,55 +1,70 @@
 package main
 
 import (
-  "fmt"
-  "net/http"
+	"fmt"
+	"html/template"
+	"log"
+	"net/http"
+	"path/filepath"
+
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
 )
 
 // Example of a Handler: homeHandle
 // Needs to create a struct and then add the ServeHTTP method to a function that has the struct as its parameter.
-type homeHandle struct {}
+type homeHandle struct{}
 
 func (h *homeHandle) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-  w.Header().Set("Content-Type", "text/html")
-  fmt.Fprint(w, "<h1>Welcome to my beautiful site! This is Handle</h1>")
+	w.Header().Set("Content-Type", "text/html")
+	fmt.Fprint(w, "<h1>Welcome to my beautiful site! This is Handle</h1>")
+}
+
+func executeTemplate(w http.ResponseWriter, filepath string) {
+	w.Header().Set("Content-Type", "text/html")
+	tpl, err := template.ParseFiles(filepath)
+	if err != nil {
+		log.Printf("parsing template: %v", err)
+		http.Error(w, "There was an error parsing the template", http.StatusInternalServerError)
+		return
+	}
+	err = tpl.Execute(w, nil)
+	if err != nil {
+		log.Printf("executing template: %v", err)
+		http.Error(w, "There was an error executing the template", http.StatusInternalServerError)
+		return
+	}
 }
 
 // Example of a HandlerFunc: home
-// The function home, because it has a ResponseWriter and a Request as its parameters, is a HandlerFunc type and already has the ServeHTTP method attached to it.
+// The function home, because it has a ResponseWriter and a Request as its parameters, can be converted to a HandlerFunc type (using HandlerFunc(home)), which will attach the ServeHTTP method to it and transform it into a Handler.
 func home(w http.ResponseWriter, r *http.Request) {
-  w.Header().Set("Content-Type", "text/html")
-  fmt.Fprint(w, "<h1>Welcome to my beautiful site! This is HandleFunc</h1>")
+	tplPath := filepath.Join("templates", "home.gohtml")
+	executeTemplate(w, tplPath)
 }
 
 func contact(w http.ResponseWriter, r *http.Request) {
-  w.Header().Set("Content-Type", "text/html")
-  fmt.Fprint(w, "To get in touch, please send an email "+
-    "to <a href=\"mailto:support@lenslocked.com\">"+ 
-    "support@lenslocked.com</a>.")
+	tplPath := filepath.Join("templates", "contact.gohtml")
+	executeTemplate(w, tplPath)
 }
 
 func faq(w http.ResponseWriter, r *http.Request) {
-  w.Header().Set("Content-Type", "text/html")
-  fmt.Fprint(w, "<h1>This is our FAQ page</h1>")
+	tplPath := filepath.Join("templates", "faq.gohtml")
+	executeTemplate(w, tplPath)
 }
 
 func user(w http.ResponseWriter, r *http.Request) {
 	userID := chi.URLParam(r, "userID")
-  w.Header().Set("Content-Type", "text/html")
-  fmt.Fprintf(w, "<h1>Here is the user id: %v</h1>", userID)
+	w.Header().Set("Content-Type", "text/html")
+	fmt.Fprintf(w, "<h1>Here is the user id: %v</h1>", userID)
 }
 
-
 func notFound(w http.ResponseWriter, r *http.Request) {
-  w.Header().Set("Content-Type", "text/html")
-  fmt.Fprint(w, "<h1>Sorry, this page doesn't exist!</h1>")
+	w.Header().Set("Content-Type", "text/html")
+	fmt.Fprint(w, "<h1>Sorry, this page doesn't exist!</h1>")
 }
 
 var nf http.Handler = http.HandlerFunc(notFound) // This is not calling http.HandlerFunc with notFound as an argument. It is actually converting the notFound function to a http.HandlerFunc type.
-
-
 
 // http.Handler - interface with the ServeHTTP method
 // http.HandlerFunc - a function type that accepts same args ServeHTTP method. Also implements http.Handler.
@@ -60,21 +75,21 @@ var nf http.Handler = http.HandlerFunc(notFound) // This is not calling http.Han
 func main() {
 	// Alternate way to implement Handle instead of initializing a homeHandle with new(homeHandle)
 	// access the value of pointer homeHandle (dereference) and assigns it to hh
-	// var hh *homeHandle 
+	// var hh *homeHandle
 	// var l http.Handler = http.HandlerFunc(logger)
 	// c := http.HandlerFunc(middleware.Logger(http.HandlerFunc(contact)))
-  r := chi.NewRouter()
+	r := chi.NewRouter()
 	r.Use(middleware.Logger)
-  r.Get("/", home)
-  // r.Handle("/homehandle", new(homeHandle))
+	r.Get("/", home)
+	// r.Handle("/homehandle", new(homeHandle))
 	// Use variable hh which implements the handle interface
 	// r.Handle("/homehandle", hh)
-  r.Get("/contact", contact)
-  r.Get("/faq", faq)
+	r.Get("/contact", contact)
+	r.Get("/faq", faq)
 	r.Get("/users/{userID}", user)
 	// r.NotFound(http.HandlerFunc(notFound))
 	r.NotFound(notFound)
-	
+
 	fmt.Println("Starting server on port 3000")
-  http.ListenAndServe(":3000", r)
+	http.ListenAndServe(":3000", r)
 }
