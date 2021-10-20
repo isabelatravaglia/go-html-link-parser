7b835dd0f24ee937e44d7507710a2eea70337c65
isabela santos <isabelatravaglia@gmail.com>
2021-10-20 17:25:51 +0000

    added views package

--- go.mod
+++ go.mod
@@ -3,10 +3,10 @@ module github.com/isabelatravaglia/go-link-parser
 go 1.16
 
 require (
-	github.com/go-chi/chi/v5 v5.0.4 // indirect
+	github.com/go-chi/chi/v5 v5.0.4
 	github.com/gorilla/mux v1.8.0
 	github.com/rogpeppe/godef v1.1.2 // indirect
 	golang.org/x/crypto v0.0.0-20200622213623-75b288015ac9 // indirect
 	golang.org/x/net v0.0.0-20210813160813-60bc85c4be6d
-	golang.org/x/tools v0.1.6 // indirect
+	golang.org/x/tools v0.1.7 // indirect
 )
--- main.go
+++ main.go
@@ -2,13 +2,13 @@ package main
 
 import (
 	"fmt"
-	"html/template"
 	"log"
 	"net/http"
 	"path/filepath"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
+	"github.com/isabelatravaglia/go-link-parser/views"
 )
 
 // Example of a Handler: homeHandle
@@ -21,19 +21,13 @@ func (h *homeHandle) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 }
 
 func executeTemplate(w http.ResponseWriter, filepath string) {
-	w.Header().Set("Content-Type", "text/html")
-	tpl, err := template.ParseFiles(filepath)
+	t, err := views.Parse(filepath)
 	if err != nil {
 		log.Printf("parsing template: %v", err)
 		http.Error(w, "There was an error parsing the template", http.StatusInternalServerError)
 		return
 	}
-	err = tpl.Execute(w, nil)
-	if err != nil {
-		log.Printf("executing template: %v", err)
-		http.Error(w, "There was an error executing the template", http.StatusInternalServerError)
-		return
-	}
+	t.Execute(w, nil)
 }
 
 // Example of a HandlerFunc: home
